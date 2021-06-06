6f4ba51c9af83c5bd160ef08d23c673d1d76243e
danchal <danchal@users.noreply.github.com>
2021-06-06 08:44:17 +0200

    allow multiple hosts

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ echo "${NUT_HOSTS}"
 
 # setup list of monitored ups hosts
 cp /etc/nut/hosts.conf.original /etc/nut/hosts.conf
-echo ${NUT_HOSTS} >> /etc/nut/hosts.conf
+
+IFS=';'
+for host in $NUT_HOSTS
+do
+    echo "${host}" >> /etc/nut/hosts.conf
+done
 
 /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
